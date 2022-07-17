e5d1cc9db9824e8fd2a14fa62db41beed624e7e6
jiadsii <45071541+jiadsii@users.noreply.github.com>
2022-07-17 16:14:31 +0800

    Update WeReadFree.js

--- js/WeReadFree.js
+++ js/WeReadFree.js
@@ -1,13 +1,6 @@
-//微信读书5.5.1以后的版本加入了验证不能使用
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.isPaying= 1;
-obj.day= 91;
-obj.remainTime= 99306196;
-obj.payingRemainTime= 3213131221;
-obj.savedMoney= 9927433;
-obj.totalFreeReadDay= 6666;
-obj.remainCoupon= 6666;
-obj.remainCount= 6666;
+
+obj.tradeEndTime = 1679685290000;
 body = JSON.stringify(obj);
 $done({body});
